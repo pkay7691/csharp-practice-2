cab046c9c8b70a69c947aa282e536ff879aa1152
Patrick Kenny <patrickmkenny91@gmail.com>
2023-06-23 11:14:19 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.Write("Hello, World!");
+// Console.WriteLine("I Love Pizza!");
+
+
+// Console.WriteLine("\tBroCode"); 
+// // This Makes a tab.  the \ initializes an escape sequence
+// Console.WriteLine("Bro\bCode");
+// Console.ReadKey();
+// // Program stops running when you type a key
+
+// int x; // delcaration
+// x = 123; // initialization
+// int y = 321; // both declaration and initialization
+// int z = x + y; //
+// int age = 32;
+// double height = 300.5;
+// // double can store decimals where int cant
+// bool alive =  true;
+// // boolean
+// char symbol = '@';
+// a single symbol
+// String name = "Bro";
+// String userName = symbol + name;
+
+
+
+// Console.WriteLine("Your age is " + age);
+// Console.WriteLine("Your height is " + height);
+// Console.WriteLine("Are you alive " + alive);
+// Console.WriteLine("Your symbol is" + symbol);
+// Console.WriteLine("Your name is  " + name);
+// Console.WriteLine("Your username is  " + userName);
+
+// Console.WriteLine(y);
+
+
+// constants = immutable 
+// const double pi = 3.14159;
+// pi = 420;
+
+// double a = 3.14;
+// int b =  Convert.ToInt32(a);
+
+// int c = 123;
+// double d = Convert.ToDouble(c);
+
+// int e = 321;
+// string f = Convert.ToString(e);
+
+// string g = "$";
+// char h = Convert.ToChar(g);
+
+// string i = "true";
+// bool j = Convert.ToBoolean(i);
+
+
+// Console.WriteLine(b.GetType());
+// Console.WriteLine(d.GetType());
+// Console.WriteLine(f.GetType());
+// Console.WriteLine(h.GetType());
+// Console.WriteLine(j.GetType());
+
+// Console.WriteLine("What's your name?");
+// string name =  Console.ReadLine();
+
+
+// Console.WriteLine("What's your age?");
+// int age = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Hello " + name);
+// Console.WriteLine("Sounds like you are " + age + " years old");
+
+// int friends  = 10;
+// friends++;
+// friends  = friends + 1;
+// friends += 1;
+
+// friends  = friends * 2;
+// friends *= 2;
+
+// friends = friends / 2;
+// friends /= 2;
+
+// int remainder = friends % 2;
+
+
+
+// Console.WriteLine(friends);
+
+
+// double x = 3.23;
+// double y = 5;
+// double a = Math.Pow(x, 2);
+// double b = Math.Sqrt(x);
+// double c =  Math.Abs(x);
+// double d = Math.Round(x);
+// double e = Math.Ceiling(x);
+// double f = Math.Floor(x);
+// double g = Math.Max(x, y);
+// double g = Math.Min(x, y);
+
+// Console.WriteLine(d);
+
+// Random random = new Random();
+
+// // int num = random.Next(1, 21);
+// double num1 = random.NextDouble();
+// double num2 = random.NextDouble();
+// double num3 = random.NextDouble();
+
+// Console.WriteLine("Enter Side A:");
+// double a = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Enter Side B:");
+// double b = Convert.ToDouble(Console.ReadLine());
+
+// double c = Math.Sqrt((a * b) + (b * b));
+
+// Console.WriteLine("The Hypotenuse is : " + c);
+
+// string fullName = "Bro Code";
+// string phoneNumber = "123-456-8902";
+// string userName = fullName.Insert(0,"@");
+// string firstName = fullName.Substring(0,3);
+// string lastName = fullName.Substring(4,4);
+
+
+// fullName = fullName.ToUpper();
+// fullName = fullName.ToLower();
+
+
+// phoneNumber = phoneNumber.Replace("-", "/");
+// Console.WriteLine(phoneNumber);
+// Console.WriteLine(userName);
+// Console.WriteLine(fullName.Length);
+// Console.WriteLine(firstName);
+// Console.WriteLine(lastName);
+
+
+Console.ReadKey();
